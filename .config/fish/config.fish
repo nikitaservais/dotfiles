90b61072d72648f501929c2406f7931925e5c07e
Nikita Servais <nikita@macbook-ns.home>
2026-03-16 10:42:42 +0100

    add changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
 set -gx EDITOR hx
+set -Ux SUDO_EDITOR hx
 
 zoxide init --cmd cd fish | source
 starship init fish | source
@@ -16,6 +17,35 @@ alias ghce="gh copilot explain"
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
 set -gx PATH $HOME/.cabal/bin /Users/nikita/.ghcup/bin $PATH # ghcup-env
 
+set -x TERM xterm-color
+set -x COLORTERM truecolor
 # Created by `pipx` on 2024-07-16 17:06:38
 set PATH $PATH /Users/nikita/.local/bin
-# enable_transience
+
+# homebrew
+set -Ux HOMEBREW_CACHE ~/.homebrew/cache
+set -Ux HOMEBREW_TEMP ~/.homebrew/temp
+
+# TODO: refactor to nix writeShellScriptBin
+set -l local_bin $HOME/.local/bin
+fish_add_path $local_bin
+if not test -f $local_bin/yz-fp
+    mkdir -p $local_bin
+    printf '#!/bin/sh\nzellij action toggle-floating-panes\nzellij action write 27\nfor selected_file in "$@"\ndo\n    zellij action write-chars ":open $selected_file"\n    zellij action write 13\ndone\nzellij action toggle-floating-panes\nzellij action close-pane\n' >$local_bin/yz-fp
+    chmod +x $local_bin/yz-fp
+end
+if not test -f $local_bin/floating-yazi
+    mkdir -p $local_bin
+    printf '#!/bin/sh\nzellij run -c -f --width 80%% --height 80%% -x 10%% -y 10%% -- yazi "$PWD"\n' >$local_bin/floating-yazi
+    chmod +x $local_bin/floating-yazi
+end
+if not test -f $local_bin/floating-lazygit
+    mkdir -p $local_bin
+    printf '#!/bin/sh\nzellij run -c -f --width 95%% --height 95%% -x 2%% -y 2%% -- lazygit\n' >$local_bin/floating-lazygit
+    chmod +x $local_bin/floating-lazygit
+end
+if not test -f $local_bin/floating-serpl
+    mkdir -p $local_bin
+    printf '#!/bin/sh\nzellij run -c -f --width 80% --height 80% -x 10% -y 10% -- serpl\n' >$local_bin/floating-serpl
+    chmod +x $local_bin/floating-serpl
+end
